67cee127d2d65fbc63d86604bdf41c8ef74bf41c
Irrelevant Byte <irrelevant.byte@gmail.com>
2022-02-24 14:53:13 +0200

    add maven example project

---
+++ app-maven-example/src/main/java/AppHelloWorld.java
@@ -0,0 +1,10 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class AppHelloWorld {
+    private static final Logger logger = LogManager.getLogger(AppHelloWorld.class);
+    public static void main(String[] args) {
+        logger.warn("Hi from log4j");
+
+    }
+}
